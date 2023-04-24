d92e06ea36a9a9db3c93b30c8457b3aeaa8688af
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-04-24 00:37:56 -0300

    chore: adds postsReducer

--- src/redux/stores.ts
+++ src/redux/stores.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import userReducer from '../actions/user';
+import postsReducer from '../actions/posts';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    posts: postsReducer,
   },
 });
 
