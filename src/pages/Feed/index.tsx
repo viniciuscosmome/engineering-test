7d20da705ad7edfc239a8ea9299ccad273417413
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-04-24 01:28:26 -0300

    feat: adds backToTop button

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useRef } from 'react';
 import { FormPost, PostSection } from '../';
 import styles from './feed.module.scss';
+import { IcArrowUp } from '../../components/icons';
 
 export function FeedPage() {
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+  const onClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 500) {
+        buttonRef.current?.classList.add(styles.show);
+      } else {
+        buttonRef.current?.classList.remove(styles.show);
+      }
+    };
+
+    document.addEventListener('scroll', onScroll);
+    return () => document.removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
     <>
       <section className={styles.section}>
@@ -9,6 +33,14 @@ export function FeedPage() {
       </section>
 
       <PostSection />
+
+      <button
+        ref={buttonRef}
+        className={`${styles.backToTop} g-button-blue`}
+        onClick={onClick}
+      >
+        <IcArrowUp />
+      </button>
     </>
   );
 }
