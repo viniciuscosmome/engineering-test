19a4dd45d80a2031cdca1c1d04ba6d7043a46778
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-04-25 04:26:40 -0300

    chore: creates responsive styles

--- src/components/forms/Textarea/index.tsx
+++ src/components/forms/Textarea/index.tsx
@@ -18,6 +18,7 @@ export const Textarea = forwardRef<HTMLTextAreaElement, iTextareaProps>((props, 
     changeButtonState
   } = props;
   const [textareaName, setTextareaName] = useState<string>(name || '');
+  const [textaresRows, setTextaresRows] = useState<number>(4);
   const [message, setMessage] = useState<string>();
   const initialValue = value;
 
@@ -35,8 +36,17 @@ export const Textarea = forwardRef<HTMLTextAreaElement, iTextareaProps>((props, 
     changeButtonState && changeButtonState(error);
   };
 
+  const changeRows = (text = ''): void => {
+    const rows = text.split('\n').length;
+
+    if (rows >= 15) setTextaresRows(14);
+    else if (rows >= 4) setTextaresRows(rows);
+    else setTextaresRows(4);
+  };
+
   const onChange = (event: FormEvent<HTMLTextAreaElement>) => {
     const textarea = event.target as HTMLTextAreaElement;
+    changeRows(textarea.value);
     validateInput(textarea);
   };
 
@@ -55,6 +65,7 @@ export const Textarea = forwardRef<HTMLTextAreaElement, iTextareaProps>((props, 
         maxLength={maxLength}
         required={required}
         onChange={onChange}
+        rows={textaresRows}
       />
 
       <div className={styles.message} data-display-error={message || ''}></div>
